be8fdee99641f9c72045d488fa5216faf5c3edde
Thiago dos Santos Vicente <thiago.santosvicente@gmail.com>
2024-10-29 00:18:07 -0300

    Novos recursos
    adicionando novo banco de dados e novas funções de backup

--- crud.py
+++ crud.py
@@ -1,33 +1,86 @@
+"""
+script criado em 28/10/2024
+script finalizado em
+
+@Autor: Thiago vicente
+"""
+
+from openpyxl import Workbook, load_workbook
 import os
+import shutil
+import threading
+import time
 
-def Criar_banco_de_dados(diretorio, nome_arquivo):
+'''
+As funções abaixo serão feitas para criar, atualizar, monitorar e fazer backup de um banco
+de dados excel (xlsx). Evitando assim problemas com o banco de dados
 
-    # Verifica se o diretório foi aberto
-    if not os.path.exists(diretorio):
-        os.makedirs(diretorio)
-        print(f"Diretório '{diretorio} criado com sucesso.")
+A primeira função do arquivo é focada em criar um diretório e um arquivo. Caso não tenha sido criado,
+ele criará o diretório e o banco de dados e emitirá uma mensagem de arquivo criado.
+Caso o diretório e o banco de dados existam. Uma mensagem de diretório e arquivo existente será exibida
 
-    # Caminho completo para o arquivo dentro do diretório
-    caminho_arquivo = os.path.join(diretorio, nome_arquivo)
+A segunda função do arquivo é focada em criar um diretório para abrigar o backup do banco de dados e fazer o primeiro backup
+Que vai evitar acidentes externos ao banco de dados , fazendo assim uma forma segura de mantê-lo salvo e o seu programa funcional
 
-    # Verificando se o arquivo existe
-    if not os.path.exists(caminho_arquivo):
-        # Criando arquivo vazio
-        with open(caminho_arquivo, 'w') as arquivo:
-            print(f"Arquivo '{caminho_arquivo}' criado com sucesso.")
+A terceira função, é parte do módulo da segunda função Onde ela sempre será invocada para fazer o backup e não termos
+que repetir o a segunda função inteira, só para poder usar parte do script
+
+
+'''
+
+def Criar_banco_de_dados(diretório, banco_de_dados):
+    # Define a extensão do arquivo
+    if not banco_de_dados.endswith('.xlsx'):
+        banco_de_dados += '.xlsx'
+
+    if not os.path.exists(diretório):
+        os.makedirs(diretório)
+        print(f"Diretório '{diretório} criado com sucesso.")
+
+    # Caminho completo para adicionar o banco de dados dentro do diretório
+    caminho_arquivo = os.path.join(diretório, banco_de_dados)
+
+    if not os.path.exists(banco_de_dados):
+        workbook = Workbook()
+        workbook.save(caminho_arquivo)
+        print(f"Diretório e arquivo '{caminho_arquivo}' criado com sucesso.")
 
     else:
-        print(f"Arquivo '{caminho_arquivo}' existente")
+        print(f"Diretório e arquivo '{caminho_arquivo}' existente.")
+
+    # Cria uma função espelho inicial
+    Criar_backup(caminho_arquivo)
 
     return caminho_arquivo
 
-# Função para fazer limpeza de tela e não ficar com excesso linhas de comando
-# Independente do sistema operacional escolhido, para operar esse programa
+def Criar_backup(caminho_arquivo):
+    dir_backup = 'backup'
+    if not os.path.exists(dir_backup):
+        os.makedirs(dir_backup)
+
+    caminho_backup = os.path.join(dir_backup, os.path.basename(caminho_arquivo))
+    shutil.copy(caminho_arquivo, caminho_backup)
+    print(f"Backup criado com sucesso em '{caminho_backup}'")
+
+    return caminho_backup
+
+def Atualizar_backup(caminho_arquivo, caminho_backup):
+    shutil.copy(caminho_arquivo, caminho_backup)
+    print(f"Backup criado com sucesso, em '{caminho_backup}'")
+
+
+
+
+
+'''
+Função criada para fazer limpeza dos arquivos evitando o excesso de informação exibida.
+Idependende do sistema operacional escolhido, para operar esse programa
+'''
 def Limpar_tela():
-    # Recurso para sistema Unix/OS X
+    # Usando recurso em sistema Unix/OS X
     if os.name == 'posix':
         _ = os.system('clear')
 
-    # Recurso para sistema windows
+    # Usando recurso em sistema Windows
     else:
-        _ = os.system('cls')
+        _= os.system('cls')
